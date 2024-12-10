b7c80054db6f4ebc2062030b0263749ed8789f19
olfimbre <olfimbre@cern.ch>
2024-12-10 22:44:50 +0100

    update

---
+++ RunAN_lpc.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+BRANCHNAME=$1
+NCORES=1
+YEARP=$2 #2016->2018
+TYPEP=$3 #MC or DATA
+SAMPLEFILENAME=$4 # ULX.txt
+OutputLabel=$5
+NFSTART=${6:-0} #FromFile
+NFEND=${7:-1} #ToFile
+SPACING_FACTOR=${8:-1.15}
+LIMIT_EDGE=${9:-1350.0}
+FIRSTBIN_LEFTEDGE=${10:-60.0}
+FIRSTBIN_RIGHTEDGE=${11:-70.0}
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=el9_amd64_gcc12
+scram project CMSSW CMSSW_13_3_0_pre4
+cd CMSSW_13_3_0_pre4/src
+echo $PWD
+cmsenv
+eval `scram runtime -sh`
+echo  "CMSSW Dir: "$CMSSW_BASE
+#git clone --branch $BRANCHNAME https://github.com/OlgaFimbresMorales/WprimeSearch.git Wprime_$YEARP
+git clone https://github.com/OlgaFimbresMorales/WprimeSearch.git Wprime_$YEARP
+WprimeDir=$PWD/Wprime_$YEARP/
+echo "Analysis Dir: "$WprimeDir
+sed -i 's/eoscms.cern.ch/cmsxrootd.fnal.gov/' $WprimeDir/proof/Selector.C
+#sed -i 's/cmsxrootd.fnal.gov/xrootd-cms.infn.it/' $WprimeDir/proof/Selector.C
+#sed -i 's/cmsxrootd.fnal.gov/xrootd.unl.edu/' $WprimeDir/proof/Selector.C
+#sed -i 's/cmsxrootd.fnal.gov/cms-xrd-global.cern.ch/' $WprimeDir/proof/Selector.C
+cd $WprimeDir/proof 
+echo $PWD
+#wget -c https://cernbox.cern.ch/files/spaces/eos/user/o/olfimbre/x509up_u154847
+wget -c https://github.com/OlgaFimbresMorales/WprimeSearch/raw/refs/heads/master/proof/x509up_u154847
+export X509_USER_PROXY=/uscms/home/omorales/x509up_u154847
+voms-proxy-info -all
+voms-proxy-info -all -file $X509_USER_PROXY
+
+cd $WprimeDir/proof/
+echo $PWD
+
+BINOPTIONS="#define SPACING_FACTOR ${SPACING_FACTOR}\n#define FIRSTBIN_LEFTEDGE ${FIRSTBIN_LEFTEDGE}\n#define FIRSTBIN_RIGHTEDGE ${FIRSTBIN_RIGHTEDGE}\n#define LIMIT_EDGE ${LIMIT_EDGE}\n"
+echo $BINOPTIONS
+
+if [ "$TYPEP" =  "MC" ]; then
+    export SAMPLEFILE=$WprimeDir/proof/files/mc/$YEARP/UL/$SAMPLEFILENAME
+    export ENTRYLISTFILE=""
+    echo -e "#define Y"$YEARP"\n#define ULSAMPLE\n${BINOPTIONS}" > IsData.h # Make sure CMSDATA is undefined
+elif [ "$TYPEP" = "DATA" ]; then
+    export SAMPLEFILE=$WprimeDir/proof/files/data/$YEARP/UL/$SAMPLEFILENAME
+    export ENTRYLISTFILE="root://cmseos.fnal.gov//store/user/avargash/WprimeSearch/proof/EntryListMaker/EntryLists_Unique.root"
+    echo -e "#define Y"$YEARP"\n#define CMSDATA\n#define ULSAMPLE\n${BINOPTIONS}" > IsData.h
+fi
+ROOTCommand="\""$OutputLabel"\",\""$SAMPLEFILE"\","$NCORES",\""$ENTRYLISTFILE"\",0,-1";
+#ROOTCommand="\""$OutputLabel"\",\""$SAMPLEFILE"\","$NCORES",\""$ENTRYLISTFILE"\", $NFSTART,$NFEND";
+root -l -b -q "Selector.C("$ROOTCommand")";
+
+
+cd $WprimeDir/proof/
+echo $PWD
+for i in `ls WprimeHistos_*.root`;
+do
+    xrdcp -vf $i root://cmseos.fnal.gov//store/user/omorales/WprimeSearchCondorOutput/$i #//cmseos.fnal.gov//store/user/avargash/WprimeSearchCondorOutput/$i
+done
